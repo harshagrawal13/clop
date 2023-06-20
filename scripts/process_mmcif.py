59c125281f51d49ce467180c3481f33073e37c87
Harsh <harshagrawal.1312@gmail.com>
2023-06-20 22:09:52 +0100

    fix: process mmcif, faster execution

--- scripts/process_mmcif.py
+++ scripts/process_mmcif.py
@@ -1,81 +1,102 @@
-"""
-Process mmCIF files and save into JSON chunks for input the model.
-"""
-
-from esm.inverse_folding.util import load_structure, extract_coords_from_structure
-import time, os
 import json
-import shutil
+import numpy as np
+import biotite
+from biotite.structure.io import pdbx
+from esm.inverse_folding.util import extract_coords_from_structure
+from biotite.structure import filter_backbone
+from biotite.structure import get_chains
 
-all_structures = []
+import time, os
+from os import path
+import shutil
+import string
+import pickle
+
 all_seqs = []
 all_prot_names = []
+all_structures = []
 
-import string
-ALL_CHAINS = list(string.ascii_uppercase)
+data_dir = os.path.join(path.abspath(path.join(__file__, "../..")), "data/")
 
-def check_and_append(complete_file_path, chain_id):
-    structure = load_structure(complete_file_path, chain_id)
-    coords, native_seq = extract_coords_from_structure(structure)
 
-    if native_seq not in all_seqs:
-        all_structures.append(coords.tolist())
-        all_seqs.append(native_seq)
-        file_name = f"{complete_file_path[-8:-4]}-{chain_id}"
-        all_prot_names.append(file_name)
+def save():
+    with open(os.path.join(data_dir, "all_seqs.json"), "w") as f:
+        json.dump(all_seqs, f, indent=4)
 
-def save_chunk(chunk_num):
-    try:
-        print("Saving chunk...", chunk_num)
-        chunk_dir = os.path.join(os.getcwd(), "data/chunks/", str(chunk_num))
-        if os.path.exists(chunk_dir):
-            shutil.rmtree(chunk_dir)
+    with open(os.path.join(data_dir, "all_prot_names.json"), "w") as f:
+        json.dump(all_prot_names, f, indent=4)
 
-        os.makedirs(chunk_dir)
-        with open(os.path.join(chunk_dir, "structure.json"), "w") as f:
-            json.dump(all_structures, f, indent=4)
-            print("Saved", len(all_structures), "structures")
-        with open(os.path.join(chunk_dir, "seq.json"), "w") as f:
-            json.dump(all_seqs, f, indent=4)
-            
-        with open(os.path.join(chunk_dir, "prot_names.json"), "w") as f:
-            json.dump(all_prot_names, f, indent=4)
+    with open(os.path.join(data_dir, "all_structures.pkl"), "wb") as f:
+        pickle.dump(all_structures, f)
 
-        all_structures.clear()
-        all_seqs.clear()
-        all_prot_names.clear()
 
-    except Exception as e:
-        print(e)
+def load_structure(fpath):
+    """
+    Args:
+        fpath: filepath to either pdb or cif file
+        chain: the chain id or list of chain ids to load
+    Returns:
+        biotite.structure.AtomArray
+    """
+    if fpath.endswith("cif"):
+        with open(fpath) as fin:
+            pdbxf = pdbx.PDBxFile.read(fin)
+        structure = pdbx.get_structure(pdbxf, model=1)
+
+    bbmask = filter_backbone(structure)
+    structure = structure[bbmask]
+    if len(structure) == 0:
+        return (None, None)
+
+    all_chains = get_chains(structure)
+    if len(all_chains) == 0:
+        raise ValueError("No chains found in the input file.")
+
+    return structure, all_chains
+
+
+def main(save_every=2000):
+    for subdir in sorted(os.listdir(os.path.join(data_dir, "mmCIF/"))):
+        complete_dir = os.path.join(data_dir, "mmCIF/", subdir)
+        expand_dir = sorted(os.listdir(complete_dir))
+
+        all_file_paths = [
+            os.path.join(complete_dir, f)
+            for f in expand_dir
+            if f.endswith(".cif")
+        ]
+
+        for file in all_file_paths:
+            structure, chain_ids = load_structure(file)
+            if chain_ids is not None:
+                for chain_id in chain_ids:
+                    try:
+                        chain_filter = [
+                            atom_.chain_id == chain_id for atom_ in structure
+                        ]
+                        coords, native_seq = extract_coords_from_structure(
+                            structure[chain_filter]
+                        )
+
+                        if native_seq not in all_seqs:
+                            all_seqs.append(native_seq)
+                            all_structures.append(coords)
+                            all_prot_names.append(f"{file[-8:-4]}-{chain_id}")
+                    except Exception as e:
+                        print(e)
+                        break
+
+            if len(all_prot_names) % save_every == 0:
+                print(f"Saving; current length = {len(all_prot_names)}")
+                save()
+
+    # Save everything at the end
+    print("Saving last chunk...")
+    save()
+
 
 if __name__ == "__main__":
-    print("Starting program...")
-
-    chunk_counter = 1
-    
-    # Change accordingly
-    chunk_size = 100
-
-    subdirs = os.listdir("data/mmcif/")
-    start_time = time.perf_counter()
-    for subdir in subdirs:
-        complete_dir = os.path.join(os.getcwd(), "data/mmcif/", subdir)
-        
-        expand_dir = os.listdir(complete_dir)
-
-        all_file_paths = [os.path.join(complete_dir, file_) for file_ in expand_dir if file_[-4:] == ".cif"]
-        
-        for file in all_file_paths:
-            for chain_id in ALL_CHAINS:
-                try:
-                    check_and_append(file, chain_id)
-                    if len(all_prot_names) == chunk_size:
-                        save_chunk(chunk_counter)
-                        chunk_counter += 1
-
-                except Exception as e:
-                    # print(e)
-                    break
-
-    finish_time = time.perf_counter()
-    print(f"Program finished in {finish_time-start_time} seconds")
+    start_time = time.time()
+    main(save_every=10000)
+    end_time = time.time()
+    print(f"Time taken = {end_time - start_time}")
